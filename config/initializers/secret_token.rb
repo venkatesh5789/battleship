bb2dfde6618912f147736efa1cdb05934c163778
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-09-27 16:59:41 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battleship::Application.config.secret_token = 'eb92b8ecc7475a2c6a421a5a89de1441fcd77dc8a2d1ada8c34a2bbcf101574725d09befdc85d4717201db6ef977fae7ce20422889e858dd8912644431a917c1'
