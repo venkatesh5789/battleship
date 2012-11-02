66eb601fa8e3dcc8b81e5d4c7ce230ed9c3427e0
Pujita Rao Narla <pujita.rao@sv.cmu.edu>
2012-11-01 22:21:27 -0400

    mess with db

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,5 +1,5 @@
 class GamePlayer < ActiveRecord::Base
-  attr_accessible :game_id, :status, :user_id
+  attr_accessible :game_id, :status, :user_id, :player_number
   belongs_to :user, class_name: "User"
   belongs_to :game, class_name: "Game"
 end
