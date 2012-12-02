2a9d9b3227dc8d4e76d18de1eece429da93cc214
Knott <wkanchan@andrew.cmu.edu>
2012-12-02 15:35:09 -0500

    Cosmetic fixes

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,5 @@
 class Chat < ActiveRecord::Base
   attr_accessible :from_user_id, :message, :game_id
-  validates :message, :format => /\A[\w\.\-\+]+\z/
   validates :message   ,presence:true
 
   belongs_to :user
